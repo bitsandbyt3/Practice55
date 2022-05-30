024b4312c82d761d4f556d56d383d68c9df6ce27
Patrik Schumann <patrik.schumann@smail.inf.h-brs.de>
2022-05-30 15:07:30 +0200

    Practice

--- main.c
+++ main.c
@@ -1,6 +1,14 @@
 #include <stdio.h>
 
 int main() {
-    printf("Hello, World!\n");
-    return 0;
+    double price = 1.45;
+
+    for (int i = 1; i <= 10; i++){
+        if (i == 1){
+            printf("Amount\t\tPrice\n");
+            printf("%6d\t%8.2f Euro\n", i, (price*i));
+        }else{
+            printf("%6d\t%8.2f Euro\n", i, (price*i));
+        }
+    }
 }
